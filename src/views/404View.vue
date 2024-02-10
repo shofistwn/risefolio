e489c614bd8c5a7acb3d61f2ed9f1dc824270ca6
Shofi Setiawan <shofistwn@gmail.com>
2024-02-10 20:30:43 +0700

    corrected a typo in the 404 page

--- src/views/404View.vue
+++ src/views/404View.vue
@@ -2,7 +2,7 @@
   <div class="mt-28 mb-52 max-w-md mx-auto text-center">
     <span class="text-9xl">404</span>
     <h2 class="text-3xl font-bold mt-3">Page Not Found</h2>
-    <p class="mt-3 mb-14">Opps! The page you are looking for does not exist. It might have been moved or delete.</p>
+    <p class="mt-3 mb-14">Oops! The page you are looking for does not exist. It might have been moved or deleted.</p>
 
     <RouterLink to="/" class="btn btn-primary">Go to <span class="text-primary">#</span>home</RouterLink>
   </div>
